f07b8342eb62f5f8cefeff582c3a13103eae582a
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-01-04 12:17:17 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name=sc.nextLine();      // sc.next() -> joe king o/p :joe
+        System.out.println(name);
+    } 
+}
