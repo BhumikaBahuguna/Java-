f5138f9da0ac1933ccfefdd63ee4a28af6d36cc8
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-02-14 08:45:28 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+class Student{
+public static void main(String args[]){
+String name=args[0];
+String uni_roll_no=args[1];
+String course=args[2];
+String semester=args[3];
+System.out.println("student details:");
+System.out.println("name:"+name);
+System.out.println("university roll no:"+uni_roll_no);
+System.out.println("course:"+course);
+System.out.println("semester:"+semester);
+}
+}
+
