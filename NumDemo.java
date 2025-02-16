71cf4ac757072686690def57248694066f911219
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-02-16 10:27:57 +0530

    Add files via upload

---
+++ NumDemo.java
@@ -0,0 +1,11 @@
+class NumDemo{
+    public static void main(String args[]){
+        byte b=126;
+        int i=15;
+        float f=22.3f;
+        double sum=0.0;
+        sum=i+f+b;
+        System.out.println("sum is:"+sum);
+        System.out.format("sum is %.3f%n",sum);
+    }
+}
