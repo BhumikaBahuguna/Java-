66f763d40eef02e21ade484538a3af0a5a62c3df
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-02-16 10:28:26 +0530

    Add files via upload

---
+++ FindHypo.java
@@ -0,0 +1,10 @@
+public class FindHypo {
+    public static void main(String args[]){
+        double d1=8.3;
+        double d2=9.1;
+        double hypo;
+        hypo=Math.sqrt(d1*d1+d2*d2);
+        System.out.println(hypo);
+
+    }
+}
