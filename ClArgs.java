90fb9330f28f22dd9181e7512e6a0a6c870c7704
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-06-02 13:29:11 +0530

    Create ClArgs.java

---
+++ ClArgs.java
@@ -0,0 +1,12 @@
+public class ClArgs{
+	public static void main(String args[]) {
+		String name = args[0];
+		String uni_no = args[1]; 
+		String course = args[2];
+		String sem = args[3];
+		System.out.println("Name is :" + name);
+		System.out.println("University roll no is :"+uni_no);
+		System.out.println("Course is:"+course);
+		System.out.println("Semester is :"+sem);
+	}
+}
